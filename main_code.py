4229af4b9aff6776c37d6023905d0b63a9ecee0a
Sina Samii <sina.sam@gmail.com>
2022-01-05 14:42:23 +0330

    one second loop

--- main_code.py
+++ main_code.py
@@ -1,13 +1,35 @@
 print('Instaloader Started!')
 import instaloader
+import sqlite3
+import time
+
 
 L = instaloader.Instaloader()
 
 # Our bot 
-USER = "anis2423h"
+USER = "anis2423j"
 
 # Target profile
+# PROFILE = 'adighodsi_creation'
 PROFILE = 'adighodsi'
+
+### databais starts
+try:
+    conn = sqlite3.connect('sqlite_{}.db'.format(PROFILE))
+    cursor = conn.cursor()
+except:
+        print("db conn error!")
+
+try:       
+    cursor.execute('''CREATE TABLE likers
+                (user text            
+                )''')
+except:
+        print("db create table error!")
+        
+
+
+
 
 # Load session previously saved with `instaloader -l USERNAME`:
 L.load_session_from_file(USER)
@@ -16,23 +38,39 @@ profile = instaloader.Profile.from_username(L.context, PROFILE)
 
 ### ----- ###
 # print a list of all the users who liked a post
-likes = []
+i = 0
 print("Fetching likes of all posts of profile {}.".format(profile.username))
 for post in profile.get_posts():
     print(post)
     for like in post.get_likes():
-        print(like.username)
-        likes = likes + [like.username]
+        a = like.username  
+        print(a)
+        
+        i = i + 1
+        print(i)
+        
+        query = '''INSERT INTO likers(
+        user) VALUES 
+        (?)'''
+        data = (a,)
+        
+        cursor.execute(query, data)
+        conn.commit()
+        time.sleep(1)
+        
 
     # one_liker = post.get_likes()
     # print(one_liker['username'])
     # likes = likes + post.get_likes()
 
-print(likes)
 
-with open("likers of a post.txt", 'w') as f:
-    for user in likes:
-        print(user, file=f)
+
+
+
+
+# with open("likers of a post.txt", 'w') as f:
+#     for user in likes:
+#         print(user, file=f)
 
 # print("Fetching followers of profile {}.".format(profile.username))
 # followers = set(profile.get_followers())
@@ -41,4 +79,6 @@ with open("likers of a post.txt", 'w') as f:
 
 # print("Storing ghosts into file.")
 
+
+conn.close()
 print('Instaloader Finished!')
